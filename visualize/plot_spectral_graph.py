1d282c3f78f2ad2953dfe900ca5795632ba436dd
dekkaiinu <youtanoboru1354@gmail.com>
2024-03-22 12:37:24 +0900

    add plot_spectral_graph.py

--- visualize/plot_spectral_graph.py
+++ visualize/plot_spectral_graph.py
@@ -1,7 +1,20 @@
+from typing import Union
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_spectral_graph(hs_pixels, ax, plot_color=None, plot_std=False):
+def plot_spectral_graph(hs_pixels: np.array, ax: plt.Axes, plot_color: Union[str, tuple] = None, plot_std: bool = False):
+    '''
+    Plot a spectral graph based on hyperspectral pixel data.
+
+    Parameters:
+        hs_pixels (np.array): Hyperspectral pixel data to plot. Each row represents a pixel, and each column represents a spectral band.
+        ax (matplotlib.axes.Axes): Axes object to plot the graph on.
+        plot_color (str or tuple, optional): Color of the plot. If not specified, default color will be used.
+        plot_std (bool, optional): Whether to plot the standard deviation as a shaded region around the average curve. Default is False.
+
+    Returns:
+        matplotlib.axes.Axes: The same Axes object after plotting the graph.
+    '''
     avg = np.average(hs_pixels, axis=0)
     std = np.std(hs_pixels, axis=0)
 
