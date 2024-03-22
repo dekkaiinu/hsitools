22fb5e919e652b8bab489c36d5b6a984d43ce875
dekkaiinu <youtanoboru1354@gmail.com>
2024-03-22 17:49:30 +0900

    fix gaussian bulr

--- correction/blur.py
+++ correction/blur.py
@@ -18,34 +18,42 @@ def hsi_blur(hsi: np.array, kernel_size: int=5):
     return smooth_hsi
 
 
-def hsi_gaussian_blur(hsi: np.array, kernel_size: int=5):
+def hsi_gaussian_blur(hsi: np.array, kernel_size: int=5, sigmaX: float=0):
     '''
     Apply Gaussian blurring to an HSI image.
 
     Parameters:
         hsi (np.array): Input HSI image.
         kernel_size (int): Size of the Gaussian kernel. Default is 5.
+        sigmaX (float): Standard deviation of the Gaussian kernel in the horizontal direction.
+            A larger value results in more smoothing. If 0, it is calculated from the kernel size.
 
     Returns:
         np.array: Smoothed HSI image.
     '''
     blur_func = cv2.GaussianBlur
-    smooth_hsi = integration_smooth_images_for_blur(hsi, blur_func, kernel_size)
+    smooth_hsi = integration_smooth_images_for_blur(hsi, blur_func, kernel_size, sigmaX)
     return smooth_hsi
 
 
-def integration_smooth_images_for_blur(hsi, blur_func, kernel_size):
+def integration_smooth_images_for_blur(hsi, blur_func, kernel_size, sigmaX=None):
     band_size = hsi.shape[2]
     if band_size % 3 == 0:
         smooth_hsi = np.empty((hsi.shape[0], hsi.shape[1], 0))
     else:
-        smooth_hsi = cv2.blur(hsi[:, :, 0], (kernel_size, kernel_size))
+        if sigmaX:
+            smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size))
+        else:
+            smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size), sigmaX)
         hsi = hsi[:, :, 1:]
 
     for i in range(int(hsi.shape[2] / 3)):
         band = i * 3
         gizi_rgb = hsi[:,:,band:band+3]
-        smooth_gizi_rgb = blur_func(gizi_rgb, (kernel_size, kernel_size))
+        if sigmaX:
+            smooth_gizi_rgb = blur_func(gizi_rgb, (kernel_size, kernel_size))
+        else:
+            smooth_gizi_rgb = blur_func(gizi_rgb, (kernel_size, kernel_size), sigmaX)
         smooth_hsi = np.dstack([smooth_hsi, smooth_gizi_rgb])
         
     return smooth_hsi
