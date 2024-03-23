3f90ac54cef2c5871df9247a7577d520829e711a
dekkaiinu <youtanoboru1354@gmail.com>
2024-03-23 12:23:52 +0900

    create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+import hsitools
+
+DESCRIPTION = 'This library is a collection of fundamental functionalities for hyperspectral image analysis'
+NAME = 'hsitools'
+AUTHOR = 'dekkaiinu'
+URL = 'https://github.com/dekkaiinu/hsitools'
+LICENSE = 'MIT '
+DOWNLOAD_URL = 'https://github.com/dekkaiinu/hsitools'
+VERSION = hsitools.__version__
+PYTHON_REQUIRES = '>=3.8.18'
+
+INSTALL_REQUIRES = [
+    'matplotlib>=3.7.5',
+    'numpy>=1.24.4',
+    'opencv-python>=4.9.0.80'
+
+]
+
+with open('README.rst', 'r') as fp:
+    readme = fp.read()
+
+setup(name=NAME,
+      author=AUTHOR,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+    )
