81b7663b63b6225aeb46a29904a7962807f8cd8c
dekkaiinu <youtanoboru1354@gmail.com>
2024-05-07 16:46:29 +0900

    fix smooth hsi

--- hsitools/correction/blur.py
+++ hsitools/correction/blur.py
@@ -18,7 +18,7 @@ def hsi_blur(hsi: np.array, kernel_size: int=5):
     return smooth_hsi
 
 
-def hsi_gaussian_blur(hsi: np.array, kernel_size: int=5, sigmaX: float=0):
+def hsi_gaussian_blur(hsi: np.array, kernel_size: int=5, sigmaX: float=1):
     '''
     Apply Gaussian blurring to an HSI image.
 
@@ -40,20 +40,42 @@ def integration_smooth_images_for_blur(hsi, blur_func, kernel_size, sigmaX=None)
     band_size = hsi.shape[2]
     if band_size % 3 == 0:
         smooth_hsi = np.empty((hsi.shape[0], hsi.shape[1], 0))
+    elif band_size % 3 == 1:
+        if sigmaX:
+            smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size), sigmaX)
+        else:
+            smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size))
+        hsi = hsi[:, :, 1:]
     else:
         if sigmaX:
-            smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size))
-        else:
             smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size), sigmaX)
-        hsi = hsi[:, :, 1:]
+            smooth_hsi = np.dstack([smooth_hsi, blur_func(hsi[:, :, 1], (kernel_size, kernel_size), sigmaX)])
+        else:
+            smooth_hsi = blur_func(hsi[:, :, 0], (kernel_size, kernel_size))
+            smooth_hsi = np.dstack([smooth_hsi, blur_func(hsi[:, :, 1], (kernel_size, kernel_size))])
+        hsi = hsi[:, :, 2:]
+
 
     for i in range(int(hsi.shape[2] / 3)):
         band = i * 3
         gizi_rgb = hsi[:,:,band:band+3]
         if sigmaX:
-            smooth_gizi_rgb = blur_func(gizi_rgb, (kernel_size, kernel_size))
-        else:
             smooth_gizi_rgb = blur_func(gizi_rgb, (kernel_size, kernel_size), sigmaX)
+        else:
+            smooth_gizi_rgb = blur_func(gizi_rgb, (kernel_size, kernel_size))
         smooth_hsi = np.dstack([smooth_hsi, smooth_gizi_rgb])
         
     return smooth_hsi
+
+from hsitools.convert import nh9_to_array
+
+
+hsi_path = '../../sample/sample_hsi.nh9'
+
+hsi_array = nh9_to_array(hsi_path)
+
+hsi_array = hsi_array[:, :, :-2]
+print(hsi_array.shape)
+
+hsi_array = hsi_gaussian_blur(hsi_array)
+print(hsi_array.shape)
